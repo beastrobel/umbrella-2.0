a97a455aefa98a9dc7ced0bc8784c6b258642727
beastrobel <beatrizstrobel@proton.me>
2024-01-13 17:42:19 -0500

    Form submit is working

--- client/src/components/navbar/Form.jsx
+++ client/src/components/navbar/Form.jsx
@@ -1,15 +1,42 @@
 import './Form.css';
-import ApiFetch from '../../utils/ApiFetch';
+import { useState } from 'react';
 
-export default function Form() { 
+function Form() {
+    const [location, setLocation] = useState('');
+
+    const handleInputChange = (e) => {
+        // Getting the value and name of the input which triggered the change
+        const { target } = e;
+        const inputValue = target.value;
+        setLocation(inputValue);
+    }    
+
+    const handleFormSubmit = (e) => {
+    e.preventDefault();
+        if (location == null) {
+            setErrorMessage('Location is invalid');
+            return;
+        }
+        console.log(`Your location: ${location}`);
+        setLocation('');
+    };
+
     return(
         <>
-        <form id="location-search" action="#location">
+        <form id="location-search" onSubmit={handleFormSubmit}>
             <label label for="search-bar">Search by city or zipcode</label>
-            <input type="text" name="postal-code" id="search-bar" onFocus="this.value=''" />
+            <input
+                value={location}
+                name="location"
+                onChange={handleInputChange}
+                type="text"
+                placeholder="location"
+                id="search-bar"
+            />
             <button type="submit" id="search-button" aria-label="search"><span className="material-symbols-outlined" id="search-bar-icon">search</span></button>
         </form>
-        <ApiFetch />
         </>
     );
-};
+}
+
+export default Form;
